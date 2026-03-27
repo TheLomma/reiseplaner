dc1a2a704436c8a4bc49229101923104a4a43bf6
TheLomma <datlomma@gmail.com>
2026-03-27 14:44:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE = "reiseplaner-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/app.jsx",
-  "/icon-180.png",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/manifest.json"
-];
-
-self.addEventListener("install", e => {
+const CACHE = 'reiseplaner-v5.2';
+const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/icon-180.png'];
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
